444e722a0c91ec264453dfc6ac821147489c79dc
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-06-01 18:35:08 +0800

    make it better'

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def dating_results():
     })
 
     # 選擇不用認證此 SSL 憑證
-    # ssl._create_default_https_context = ssl._create_unverified_context
+    ssl._create_default_https_context = ssl._create_unverified_context
 
     with req.urlopen(request) as response:
         data=response.read().decode("utf-8")
