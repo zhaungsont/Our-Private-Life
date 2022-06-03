3de81c0cc8f04b2d45a888cc308c0d44bd910f6f
unachen <08811007@gm.scu.edu.tw>
2022-06-03 22:39:00 +0800

    Summary

--- app.py
+++ app.py
@@ -134,6 +134,7 @@ def booking():
     date2= flask_request.form.get('check_out_date')
     ad = flask_request.form.get('ad')
     ch = flask_request.form.get('ch')
+    age = flask_request.form.get('age')
     room = flask_request.form.get('room')
     #在終端機輸出 確保資料抓取正確
     print(year1)
@@ -144,9 +145,11 @@ def booking():
     print(date2)
     print(ad)
     print(ch)
+    print(age)
     print(room)
+    
     #將變數帶入 url 內 並用此 url 進行爬蟲
-    url=f"https://www.booking.com/searchresults.zh-tw.html?label=booking-name-yefrPbbyS*FIINHgyCnmNgS267725091255%3Apl%3Ata%3Ap1%3Ap22%2C563%2C000%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp1012825%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfqnDqqG8nt1O4nYvDr1lms&sid=f403d2b3a73243d32c27dbfc3fa9f606&aid=376396&ss=%E5%8F%B0%E5%8C%97&ssne=%E5%8F%B0%E5%8C%97&ssne_untouched=%E5%8F%B0%E5%8C%97&lang=zh-tw&sb=1&src_elem=sb&dest_id=-2637882&dest_type=city&checkin={year1}-{month1}-{date1}&checkout={year2}-{month2}-{date2}&group_adults={ad}&no_rooms={room}&group_children={ch}&sb_travel_purpose=leisure&order=class"
+    url=f"https://www.booking.com/searchresults.zh-tw.html?label=booking-name-yefrPbbyS*FIINHgyCnmNgS411022708271%3Apl%3Ata%3Ap1%3Ap22%2C563%2C000%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp9040379%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfqnDqqG8nt1O4nYvDr1lms&sid=a54a62923cbb9d7afe34b34af03d7ecd&aid=376396&ss=%E5%8F%B0%E5%8C%97&ssne=%E5%8F%B0%E5%8C%97&ssne_untouched=%E5%8F%B0%E5%8C%97&lang=zh-tw&sb=1&src_elem=sb&dest_id=-2637882&dest_type=city&checkin={year1}-{month1}-{date1}&checkout={year2}-{month2}-{date2}&group_adults={ad}&no_rooms={room}&group_children={ch}&age={age}&sb_travel_purpose=leisure&order=class"
     print("***********")
     print(url)
     print("***********")
@@ -158,6 +161,8 @@ def booking():
     request = req.Request(url, headers={
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
     })
+
+
     #取的網頁的原始碼 並用decode("utf-8")來翻譯成中文
     with req.urlopen(request) as response:
         data = response.read().decode("utf-8")
@@ -166,15 +171,17 @@ def booking():
     #抓取對應標籤之資料
     title_and_img = root.find_all("img", class_=title_and_img_class)
     prices = root.find_all("span", class_=price_class)
-    order_portals = root.find_all("a", class_=order_url_class)
+
     money=root.find_all("span",class_='fcab3ed991 bd73d13072')
     score=root.find_all('div',class_='b5cd09854e d10a6220b4')
+
     #設定空list
     imgurl_list = []
     title_list = []
     results = []
     money_text=[]
     score_text=[]
+    order_url=[]
 
     #將變數 money 資料放入 money_text list
     for M in money:
@@ -186,6 +193,11 @@ def booking():
     for entry in title_and_img:
         imgurl_list.append(entry['src'])
         title_list.append(entry['alt'])
+    #抓取飯店連結
+    sel = root.find_all("a",class_=order_url_class)
+    for s in sel:
+        bkurl=str(s).split("href=\"")[1].split("\" rel")[0]
+        order_url.append(bkurl)
     #抓取價錢資料
     money_list=[]
     #根據 money_text 長度判斷跑幾次 將\xa0取代為空白
@@ -208,15 +220,21 @@ def booking():
         score_list=score_list+[score_n]
     #將所有抓取到的資料list 照順序存進字典 new_entry
     for i in range(len(imgurl_list)):
-            new_entry = {}
-            new_entry['title'] = title_list[i]
-            new_entry['img'] = imgurl_list[i]
+        new_entry = {}
+        new_entry['title'] = title_list[i]
+        new_entry['img'] = imgurl_list[i]
+        new_entry['order'] = order_url[i]
+        try:
             new_entry['price'] = prices[i].string
-            new_entry['order'] = order_portals[i]['href']
+        except:
+            new_entry['price'] = '點擊連結以觀看'
+        try:
             new_entry["score"] =score_list[i]
-            results.append(new_entry)
+        except:
+            new_entry["score"] ='尚無評價'
+        results.append(new_entry)
     #將資料return到booking.html        
-    return render_template('booking.html', data=results, year1=year1, month1=month1, date1=date1, year2=year2, month2=month2, date2=date2, ad=ad, ch=ch, room=room)
+    return render_template('booking.html', data=results, year1=year1, month1=month1, date1=date1, year2=year2, month2=month2, date2=date2, ad=ad, ch=ch, age=age, room=room)
 
 
 @app.route('/gifting')
