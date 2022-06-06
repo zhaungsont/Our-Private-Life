254001cfa37e384ba8d913acb6f9ab7e36ccb363
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-06-06 11:22:16 +0800

    add urllib header info

--- app.py
+++ app.py
@@ -84,7 +84,8 @@ def dating_results():
     #爬html
     request=req.Request(url,headers={
         #若網站有cookie，也是放這
-        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
+        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
     })
 
     # 選擇不用認證此 SSL 憑證
