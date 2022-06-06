e9a98d2b7e75c9cbb950338a5876311ed6c8e537
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-06-06 11:44:04 +0800

    change headers to hdrs

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def dating_results():
     print(f'url={url}')
 
     #爬html
-    request=req.Request(url,headers={
+    request=req.Request(url,hdrs={
         #若網站有cookie，也是放這
         "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36",
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
